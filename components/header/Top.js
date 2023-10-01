38c14bebde7dd0c947dce60d49a0aa1c0ac37e66
Aaditya1998 <61281583+Aaditya1998@users.noreply.github.com>
2023-10-01 23:57:30 +0530

    First Commit

---
+++ components/header/Top.js
@@ -0,0 +1,69 @@
+import styles from "./styles.module.scss";
+import { MdSecurity } from "react-icons/md"; // Import the MdSecurity component correctly
+import { BsSuitHeart } from "react-icons/bs";
+import { RiAccountPinCircleLine, RiArrowDropDownFill } from "react-icons/ri";
+import Link from "next/link";
+import { useState } from "react";
+import { UserMenu } from "./UserMenu";
+
+export function Top() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  return (
+    <div className={styles.top}>
+      <div className={styles.top__container}>
+        <div> </div>
+        <ul className={styles.top__list}>
+          <li className={styles.li}>
+            <div className={styles.iconTextWrapper}>
+              <img
+                src="https://i.ibb.co/NLxL4Qj/Round-indian-flag-png.png"
+                alt=""
+              />
+              <span>India / inr</span>
+            </div>
+          </li>
+          <li className={styles.li}>
+            <div className={styles.iconTextWrapper}>
+              <MdSecurity />
+              <span>Buyer Protection</span>
+            </div>
+          </li>
+          <li className={styles.li}>
+            <span>Customer Service</span>
+          </li>
+          <li className={styles.li}>
+            <span>Help</span>
+          </li>
+          <li className={styles.li}>
+            <div className={styles.iconTextWrapper}>
+              <BsSuitHeart />
+              <Link href="/profile/wishlist">
+                <span>Wishlist</span>
+              </Link>
+            </div>
+          </li>
+          <li className={styles.li}>
+            {loggedIn ? (
+              <li className={styles.li}>
+                <div className={`${styles.flex} ${styles.iconTextWrapper}`}>
+                  <img src="https://i.ibb.co/HPczpxR/pngwing-com.png" alt="" />
+                  <span>Aditya</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            ) : (
+              <li className={styles.li}>
+                <div className={`${styles.flex} ${styles.iconTextWrapper}`}>
+                  <RiAccountPinCircleLine />
+                  <span>Account</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            )}
+            <UserMenu loggedIn={loggedIn} />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.scss";
+import { Provider } from "react-redux";
+import store from "../store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import Head from "next/head";
+let persistor = persistStore(store);
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Oya</title>
+        <meta
+          name="description"
+          content="Oya: A ecommerse store for Electronics related to Battories, Electric Vehicals and Chargers"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate Loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
+  );
 }
+export default MyApp;
